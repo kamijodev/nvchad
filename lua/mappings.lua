175d764087c95cb0e5d633a530c5f8554da0783b
kamijodev <kamijodev@gmail.com>
2024-05-29 12:52:34 +0900

    setting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,140 @@
 require "nvchad.mappings"
 
--- add yours here
+local del_mappings = {
+  i = {},
+  n = {"<leader>h", "<leader>v", "<leader>wk", "<leader>x", "<leader>wK", "<leader>n", "<leader>rn", "<leader>/" },
+  v = {},
+}
 
-local map = vim.keymap.set
+local mappings = {
+  n = {
+    -- avoid saving to register
+    { "x", '"_x' },
+    { "X", '"_D' },
+    { "dx", '"_dd' },
+    { "s", "\"_s" },
+    -- set mapping
+    { "<leader><leader>m", ":e ~/.config/nvim/lua/mappings.lua <cr>" },
+    -- vscode like line shift
+    { "<c-j>", ':m .+1<cr>=='},
+    { "<c-k>", ':m .-2<cr>=='},
+    -- close
+    { "<c-d-q>", ':q <cr>'},
+    -- tab control
+    {
+      "<c-l>",
+      function()
+        require("nvchad.tabufline").next()
+      end,
+      { desc = "Buffer Goto next" },
+    },
+    {
+      "<c-h>",
+      function()
+        require("nvchad.tabufline").prev()
+      end,
+      { desc = "Buffer Goto prev" },
+    },
+    {
+      "<c-/>",
+      function()
+        require("nvchad.tabufline").close_buffer()
+      end,
+      { desc = "Buffer Close" },
+    },
+    -- comment
+    {
+      "gc",
+       function()
+         require("Comment.api").toggle.linewise()
+       end, { desc = "Comment Toggle" }
+    },
+    -- subversive
+    { "<leader>r", "<Plug>(SubversiveSubstitute)", { noremap = true } },
+    -- nvimtree
+    {
+      "<c-n>",
+      function()
+        require("nvim-tree.api").tree.toggle({
+          find_file = true,
+          update_root = true
+        })
+      end,
+    },
+    -- terminal
+    {
+      "<c-s><c-j>",
+      function()
+        require("nvchad.term").toggle { pos = "sp", id = "bottom1" }
+        require("nvchad.term").toggle { pos = "vsp", id = "bottom2", size = 0.5 }
+      end;
+    },
+    {
+      "<c-s><c-l>",
+      function()
+        require("nvchad.term").toggle { pos = "vsp", id = "right1" }
+      end
+    },
+    -- tab
+    { "<c-s><c-n>", "<cmd>tabnew<cr>" },
+    { "<c-s><c-c>", "<cmd>tabclose<cr>" },
+    { "<c-s><c-l>", "<cmd>+tabnext<cr>" },
+    { "<c-s><c-h>", "<cmd>-tabnext<cr>" },
+    -- etc
+    { "<", "<<" },
+    { ">", ">>" },
+    { "L", "$" },
+    { "H", "^" },
+    { "<c-s><c-s>", ":w<cr>"},
+  },
+  v = {
+    -- avoid saving to register
+    { "x", '"_x' },
+    { "s", '"_s' },
+    -- vscode like line shift
+    {"<c-n>", ":m '>+1<cr>gv=gv"},
+    {"<c-e>", ":m '<-2<cr>gv=gv"},
+    -- return to the original position
+    {"y", "ygv<esc>"},
+    -- comment
+    {
+      "gc", "<Plug>(comment_toggle_linewise_visual)"
+    },
+  },
+  t = {
+    -- { "<c-s><c-k>",
+    --   function()
+    --     require("nvchad.term").toggle { pos = "sp", id = "bottom1" }
+    --   end
+    -- },
+    {
+      "<c-s><c-k>",
+      function()
+        require("nvchad.term").toggle { pos = "sp", id = "bottom1" }
+        require("nvchad.term").toggle { pos = "vsp", id = "bottom2" }
+      end
+    },
+    { "<c-s><c-h>", "<cmd>winc h<cr>" },
+    { "<c-s><c-l>", "<cmd>winc l<cr>" },
+    -- { "<c-s><c-n>", "<cmd>tabnew<cr>" },
+    -- { "<c-s><c-q>", "<cmd>tabclose<cr>" },
+    -- { "<c-s><c-l>", "<cmd>+tabnext<cr>" },
+    -- { "<c-s><c-h>", "<cmd>-tabnext<cr>" },
+    {
+      "<esc>", "<C-\\><C-n>:"
+    }
+  }
+}
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+for mode, mode_mappings in pairs(mappings) do
+  for _, map in ipairs(mode_mappings) do
+    local lhs, rhs, opts = map[1], map[2], map[3]
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+for mode, command in pairs(del_mappings) do
+  for i = 1, #command do
+    vim.keymap.del(mode, command[i])
+  end
+end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,15 +6,41 @@ return {
       require "configs.conform"
     end,
   },
+  {
+    "folke/which-key.nvim",
+    enabled = false
+  },
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      mappings = {
+        basic = false,
+        extra = false
+      },
+
+    },
+    lazy = false
+  },
+  {
+    "svermeulen/vim-subversive",
+    lazy = false
+  },
+  {
+    "andymass/vim-matchup",
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+    lazy = false
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
   --
   -- {
   -- 	"williamboman/mason.nvim",
